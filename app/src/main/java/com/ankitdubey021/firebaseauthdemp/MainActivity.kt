9c120e4f67b630608fd346fa82ba6a9ef4631031
Ankit <ankit.dubey@gojek.com>
2024-11-03 01:08:23 +0530

    Google SignIN with OAuth

---
+++ app/src/main/java/com/ankitdubey021/firebaseauthdemp/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.ankitdubey021.firebaseauthdemp
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+class MainActivity : AppCompatActivity() {
+
+    private var googleSignInClient: GoogleSignInClient? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            setup()
+        }
+    }
+
+    private fun setup() {
+        Log.e("Tiger", "Setup")
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("151791698402-ab5i5h8b175g526m8dskl16vrrjf5qg2.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInIntent = googleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account.idToken!!, onSuccess = {
+                val email = account.email
+                findViewById<TextView>(R.id.tv).text = email
+            })
+        } catch (e: ApiException) {
+            // Handle sign-in failure
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String, onSuccess: () -> Unit) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    onSuccess()
+                    Log.e("Tiger", "Success")
+                    signOut()
+                } else {
+                    Log.e("Tiger", "Failure")
+                    // Sign-in failure
+                }
+            }
+    }
+
+    private fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        googleSignInClient?.signOut()?.addOnCompleteListener {}
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+    }
+
+}
